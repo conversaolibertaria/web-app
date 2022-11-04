7f146184760e844676f787cc9040dfb42e46bdc3
eber404 <bodacamargo@gmail.com>
2022-11-04 20:27:26 +0000

    Deploying to gh-pages from @ conversaolibertaria/web-app@f617d25b11298ddcb203e66ff08ea03da33463ab 🚀

---
+++ _next/static/fhogjfxNazM-7RtqBF0Jd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8034fd035ae25f0f.css","static/chunks/pages/index-113a29c4557129d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
